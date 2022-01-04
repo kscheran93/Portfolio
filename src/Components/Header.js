35fc6b38e26d3aece7743037a7bf5dbe944a816b
kscheran93 <57915456+kscheran93@users.noreply.github.com>
2022-01-04 08:34:56 +0000

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+
+    if(this.props.data){
+      var name = this.props.data.name;
+      var occupation= this.props.data.occupation;
+      var description= this.props.data.description;
+      var city= this.props.data.address.city;
+      var networks= this.props.data.social.map(function(network){
+        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+      })
+    }
+
+    return (
+      <header id="home">
+        <nav id="nav-wrap">
+          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+            Show navigation
+          </a>
+          <a className="mobile-btn" href="#home" title="Hide navigation">
+            Hide navigation
+          </a>
+
+          <ul id="nav" className="nav">
+            <li className="current">
+              <a className="smoothscroll" href="#home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#about">
+                About
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#resume">
+                Resume
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#portfolio">
+                Works
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#testimonials">
+                Testimonials
+              </a>
+            </li>
+            <li>
+              <a className="smoothscroll" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="row banner">
+          <div className="banner-text">
+            <h1 className="responsive-headline">{name}</h1>
+            <h3>
+              "Competent UI Develper/React Developer professional with 2 years of experience and skills in achieving greater organizational efficiency & profitability."
+            </h3>
+            <h3>React|Node.js|JS|SQL|MongoDB|JQuery|Boostrap5|CSS3|HTML5</h3>
+            <h3>
+              <a className='site_link' href="https://amazon-clone93.netlify.app/">
+                Cloned Amazon Shopping Website in 5 Days
+              </a>
+            </h3>
+            <h3>
+              <a className='site_link' href="https://tesla-clone-reactjs-frontpage.netlify.app/">
+                Cloned Tesla in 2 Days
+              </a>
+            </h3>
+            <hr />
+            <ul className="social">{networks}</ul>
+          </div>
+        </div>
+
+        <p className="scrolldown">
+          <a className="smoothscroll" href="#about">
+            <i className="icon-down-circle"></i>
+          </a>
+        </p>
+      </header>
+    );
+  }
+}
+
+export default Header;
