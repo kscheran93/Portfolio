47e00c47ef454395869c40fb0ba10b54882d3150
kscheran93 <57915456+kscheran93@users.noreply.github.com>
2022-08-05 20:45:35 +0100

    feat: :art: add the new company details
    I have made some change on new work component

---
+++ build/precache-manifest.3a8a908f87fff57e13c11ce562bd7f24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb741a5a317eaa7695ce78e4de1de6c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6d91f7d0d59688f85ff",
+    "url": "/static/js/2.d18ad423.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.d18ad423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87f7123ea965e34250b3",
+    "url": "/static/js/main.fb6796e7.chunk.js"
+  },
+  {
+    "revision": "eb378f7fa9cb0f8ea242",
+    "url": "/static/js/runtime-main.b56de62c.js"
+  }
+]);
